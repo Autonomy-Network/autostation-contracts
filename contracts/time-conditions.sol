d6b6f3a0aeb99c24da956a7eab7356f35ed0f8e6
pldespaigne <pl.despaigne@gmail.com>
2022-02-03 13:22:02 +0100

    requested changes & reccuring tx

--- contracts/time-conditions.sol
+++ contracts/time-conditions.sol
@@ -6,12 +6,14 @@ pragma solidity ^0.8.11;
 
 contract TimeConditions {
 
+  mapping(address => mapping(uint256 => uint256)) public userToIDtoLastExecTime;
+
   function executeAt(
     uint256 notBeforeTime,
     uint256 notAfterTime,
     address payable target,
     bytes memory params
-  ) public payable returns(bool, bytes memory) {
+  ) public payable returns(bool success, bytes memory returnedData) {
 
     /* solhint-disable not-rely-on-time */
     require(block.timestamp > notBeforeTime, "Error: too earlly!");
@@ -19,9 +21,27 @@ contract TimeConditions {
     /* solhint-enable not-rely-on-time */
 
     // execute target function
-    (bool success, bytes memory returnedData) = target.call{value: msg.value}(params); // solhint-disable-line indent, bracket-align
+    // solhint-disable-next-line indent, bracket-align, avoid-low-level-calls
+    (success, returnedData) = target.call{value: msg.value}(params);
+  }
 
-    return (success, returnedData);
+  function executeReccuring(
+    address originalSender,
+    uint256 callID,
+    uint256 secondsBetweenCalls,
+    address payable target,
+    bytes memory params
+  ) public payable returns(bool success, bytes memory returnedData) {
+
+    uint256 nextExecTime = userToIDtoLastExecTime[originalSender][callID] + secondsBetweenCalls;
+    require(block.timestamp > nextExecTime, "Error: too earlly!"); // solhint-disable-line not-rely-on-time
+
+    // update last execution time
+    userToIDtoLastExecTime[originalSender][callID] += secondsBetweenCalls;
+
+    // execute target function
+    // solhint-disable-next-line indent, bracket-align, avoid-low-level-calls
+    (success, returnedData) = target.call{value: msg.value}(params);
   }
 
 }
